1dc7143c26668352dab766d233165e86c8cc90fa
Sumanbs <39578973+Sumanbs@users.noreply.github.com>
2018-12-06 17:17:13 +0530

    Add files via upload

---
+++ Fundoo/src/app/Services/Constants/constants.ts
@@ -0,0 +1,5 @@
+export class socialLibraryConstants {
+    static facebookID = "1261875137320900";
+    static googleID =
+        "198946252969-hepq58tqbqpfu3m10avj8k0nql6hnj4m.apps.googleusercontent.com";
+}
