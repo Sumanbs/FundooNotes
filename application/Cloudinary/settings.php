74e6ddf6c359f4d2b68e8e32537ff177a3cb2526
Sumanbs <suman789014@gmail.com>
2018-12-07 14:31:40 +0530

    update

--- application/Cloudinary/settings.php
+++ application/Cloudinary/settings.php
@@ -2,6 +2,6 @@
 include "/var/www/html/codeigniter/application/Cloudinary/vendor/cloudinary/cloudinary_php/src/Cloudinary.php";
 \Cloudinary::config(array(
     "cloud_name" => "drcvycuvh",
-    "api_key"    => "181364185741832",
-    "api_secret" => "2A6jbPYDV2cmNLu9f4fSwaQw6Gs",
+    "api_key"    => "sdf",
+    "api_secret" => "dvs",
 ));
